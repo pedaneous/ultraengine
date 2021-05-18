818dca0e6b98bfdda0312889b62bd2090a210492
gilbert <67091311+pedaneous@users.noreply.github.com>
2021-05-18 15:10:53 -0400

    Create Vector2.java

---
+++ UltraEngine/src/UltraEngine/AddOns/Vector2.java
@@ -0,0 +1,18 @@
+class Vector2 {
+	public double x,y;
+	
+	public Vector2(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void add(double x, double y){
+		this.x += x;
+		this.y += y;
+	}
+	
+	public void add(Vector2 vector){
+		this.x += vector.x;
+		this.y += vector.y;
+	}
+}
